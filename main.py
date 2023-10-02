9ef296f14d0c3f3bc857e77b544a0b0f383ce280
wolf4605 <125435168+wolf4605@users.noreply.github.com>
2023-10-02 13:45:08 +0000

    Initital Commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import discord
 from discord.ext import commands
+import settings
 
-
-print (configurations.Prefix)
+print (settings.Prefix)
 class Mew(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         intents.message_content = True
-        super().__init__(command_prefix=configurations.PREFIX, intents=intents)
+        super().__init__(command_prefix=settings.PREFIX, intents=intents)
