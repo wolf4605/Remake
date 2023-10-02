51b8bce6120dbca9cd23e93bdaf5757a9e27ba3f
wolf4605 <125435168+wolf4605@users.noreply.github.com>
2023-10-02 13:46:42 +0000

    Initial commit

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import discord
 from discord.ext import commands
 import settings
 
-print (settings.Prefix)
+print (f'settings.Prefix')
 class Mew(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         intents.message_content = True
-        super().__init__(command_prefix=settings.PREFIX, intents=intents)
+        super().__init__(command_prefix=settings.Prefix, intents=intents)
+
+    async def setup_hook(self):
+        print('\033[31m Hello ')
