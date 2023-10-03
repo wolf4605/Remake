58ee1323c551d76e3d1dbdb0d33563ed2de40762
wolf4605 <125435168+wolf4605@users.noreply.github.com>
2023-10-03 05:22:43 +0000

    Added Links to images for fun commands

---
+++ asset/image_links.py
@@ -0,0 +1,169 @@
+BITE = ["https://cdn.discordapp.com/attachments/1097837629833224192/1099230277454286888/anime-bite_1.gif",
+        "https://cdn.discordapp.com/attachments/1097837629833224192/1099230277764661248/anime-bite_2.gif",
+        "https://cdn.discordapp.com/attachments/1097837629833224192/1099230278070837328/anime-bite_3.gif",
+        "https://cdn.discordapp.com/attachments/1097837629833224192/1099230278418976788/anime-bite_4.gif",
+        "https://cdn.discordapp.com/attachments/1097837629833224192/1099230278725156874/anime-bite.gif",
+        "https://cdn.discordapp.com/attachments/1097837629833224192/1099230279098454086/anime-cute.gif",
+        "https://cdn.discordapp.com/attachments/1097837629833224192/1099230279488520253/bite.gif",
+        "https://cdn.discordapp.com/attachments/1097837629833224192/1099230279857623090/re-zero-rem.gif"
+        ]
+
+BRO_KISS = ["https://cdn.discordapp.com/attachments/1099218788362223716/1099219008626110524/diabolik-lovers-anime.gif",
+            "https://cdn.discordapp.com/attachments/1099218788362223716/1099219008961663076/gay-anime.gif",
+            "https://cdn.discordapp.com/attachments/1099218788362223716/1099219009297195049/gay-kiss.gif",
+            "https://cdn.discordapp.com/attachments/1099218788362223716/1099219009645326336/gay-tall-kiss.gif",
+            "https://cdn.discordapp.com/attachments/1099218788362223716/1099219010006044753/kiss-anime.gif",
+            "https://cdn.discordapp.com/attachments/1099218788362223716/1099219010316410980/surrender-kiss.gif"
+]
+
+GIRL_KISS = ["https://cdn.discordapp.com/attachments/1099218931824205864/1099219792893857872/kiss-anime.gif",
+            "https://cdn.discordapp.com/attachments/1099218931824205864/1099219793279729824/lesbian_1.gif",
+            "https://cdn.discordapp.com/attachments/1099218931824205864/1099219793657204837/lesbian.gif",
+            "https://cdn.discordapp.com/attachments/1099218931824205864/1099219794059874385/sakura-trick-yuu.gif",
+            "https://cdn.discordapp.com/attachments/1099218931824205864/1099219794399608904/yuri-kiss-yuri.gif",
+]
+
+HUG = ["https://cdn.discordapp.com/attachments/1097782488392273920/1098162907541422152/Hug.gif",
+            "https://cdn.discordapp.com/attachments/1097782488392273920/1098162899958108230/Hug_4.gif",
+            "https://cdn.discordapp.com/attachments/1097782488392273920/1098162851790733332/hug-hugs.gif",
+            "https://cdn.discordapp.com/attachments/1097782488392273920/1098162848330428526/Hug_3.gif",
+            "https://cdn.discordapp.com/attachments/1097782488392273920/1098162801199026296/chobits-hideki-motosuwa.gif",
+            "https://cdn.discordapp.com/attachments/1097782488392273920/1098162797638078524/Hug_2.gif",
+            "https://cdn.discordapp.com/attachments/1097782488392273920/1098162754411560980/anime-hug.gif",
+            "https://cdn.discordapp.com/attachments/1097782488392273920/1098162662946385930/anime-hug_1.gif",
+            "https://cdn.discordapp.com/attachments/1097782488392273920/1098162656487157860/anime-hug_2.gif",
+            "https://cdn.discordapp.com/attachments/1097782488392273920/1098162554511044618/anime-happy.gif",
+]
+
+KICK = ["https://cdn.discordapp.com/attachments/1097833659463118908/1098186462622666853/anime-dress.gif",
+        "https://cdn.discordapp.com/attachments/1097833659463118908/1098186462933033040/anime-imouto.gif",
+        "https://cdn.discordapp.com/attachments/1097833659463118908/1098186463432147026/emilia-fight.gif",
+        "https://cdn.discordapp.com/attachments/1097833659463118908/1098186463981621318/kick.gif",
+        "https://cdn.discordapp.com/attachments/1097833659463118908/1098186464531062784/kick-anime.gif",
+        "https://cdn.discordapp.com/attachments/1097833659463118908/1098186465034371093/kick-in-the-balls-anime.gif",
+        "https://cdn.discordapp.com/attachments/1097833659463118908/1098186465546096730/penitencia-kick.gif",
+        "https://cdn.discordapp.com/attachments/1097833659463118908/1098186465986486282/shida-midori-midori.gif",
+        "https://cdn.discordapp.com/attachments/1097833659463118908/1098186466443657226/taiga-aisaka-starling-bg-waifu.gif",
+        "https://cdn.discordapp.com/attachments/1097833659463118908/1098186466988937227/voz-dap-chym-dap-chym.gif",
+]
+
+KILL = ["https://cdn.discordapp.com/attachments/1097782406276190309/1098094079738392627/giphy.gif",
+        "https://cdn.discordapp.com/attachments/1097782406276190309/1098150399741014097/anime-wasted.gif",
+        "https://cdn.discordapp.com/attachments/1097782406276190309/1098151524976308224/anime-wasted_1.gif",
+        "https://cdn.discordapp.com/attachments/1097782406276190309/1098152404563787816/wasted-haikyuu.gif",
+        "https://cdn.discordapp.com/attachments/1097782406276190309/1098152562387071047/anime-girl.gif",
+        "https://cdn.discordapp.com/attachments/1097782406276190309/1098152867254239333/anime-wasted_2.gif",
+]
+
+KISS = ["https://cdn.discordapp.com/attachments/1099221779165237258/1099221982672863303/anime-kiss_1.gif",
+        "https://cdn.discordapp.com/attachments/1099221779165237258/1099221983201349683/anime-kiss.gif",
+        "https://cdn.discordapp.com/attachments/1099221779165237258/1099221983587217448/engage-kiss-anime-kiss.gif",
+        "https://cdn.discordapp.com/attachments/1099221779165237258/1099221983994069032/kiss.gif",
+        "https://cdn.discordapp.com/attachments/1099221779165237258/1099221984434475008/kiss-love.gif",
+        "https://cdn.discordapp.com/attachments/1099221779165237258/1099221984845508658/nagumi-koushi-hozumi-serene.gif",
+]
+
+LAUGH = ["https://cdn.discordapp.com/attachments/1097782632139468842/1098181103271022613/emilia-re-zero.gif",
+        "https://cdn.discordapp.com/attachments/1097782632139468842/1098181103761772624/fairy-tail-natsu.gif",
+        "https://cdn.discordapp.com/attachments/1097782632139468842/1098181104210546718/laugh-anime_1.gif",
+        "https://cdn.discordapp.com/attachments/1097782632139468842/1098181104588050452/laugh-anime.gif",
+        "https://cdn.discordapp.com/attachments/1097782632139468842/1098181105074573392/laugh-giggle.gif",
+        "https://cdn.discordapp.com/attachments/1097782632139468842/1098181105590485075/laughing-lol.gif",
+        "https://cdn.discordapp.com/attachments/1097782632139468842/1098181106051862528/nichijou-hahaha.gif",
+        "https://cdn.discordapp.com/attachments/1097782632139468842/1098181106362224641/rezero-rem.gif",
+        "https://cdn.discordapp.com/attachments/1097782632139468842/1098181106714558494/tomoko-kuroki.gif",
+]
+
+PAT = ["https://cdn.discordapp.com/attachments/1097837608652001300/1098269282103730186/pat.gif",
+        "https://cdn.discordapp.com/attachments/1097837608652001300/1098269281663332382/pat_1.gif",
+        "https://cdn.discordapp.com/attachments/1097837608652001300/1098269281218727996/kanna-kamui-pat.gif",
+        "https://cdn.discordapp.com/attachments/1097837608652001300/1098269280874799234/horimiya-pat-pat-anime.gif",
+        "https://cdn.discordapp.com/attachments/1097837608652001300/1098269280421818419/emi-emilia.gif",
+        "https://cdn.discordapp.com/attachments/1097837608652001300/1098269280035938325/anime-pat.gif",
+        "https://cdn.discordapp.com/attachments/1097837608652001300/1098269279226441739/anime-pat_1.gif",
+        "https://cdn.discordapp.com/attachments/1097837608652001300/1098269278668595200/anime-good-girl.gif",
+        "https://cdn.discordapp.com/attachments/1097837608652001300/1098269278085582899/aharensan-aharen.gif",
+]
+
+POKE = ["https://cdn.discordapp.com/attachments/1099140487975276675/1099251937423138927/anime-poke.gif",
+        "https://cdn.discordapp.com/attachments/1099140487975276675/1099251937813217371/anime-sleep.gif",
+        "https://cdn.discordapp.com/attachments/1099140487975276675/1099251938215854081/boob-poke.gif",
+        "https://cdn.discordapp.com/attachments/1099140487975276675/1099251938547212288/boop-anime.gif",
+        "https://cdn.discordapp.com/attachments/1099140487975276675/1099251938886959115/poke_1.gif",
+        "https://cdn.discordapp.com/attachments/1099140487975276675/1099251939251847249/poke_2.gif",
+        "https://cdn.discordapp.com/attachments/1099140487975276675/1099251939683876864/poke.gif",
+]
+
+POUT = ["https://cdn.discordapp.com/attachments/1099140243724177579/1099255409933549589/noela-angry.gif",
+        "https://cdn.discordapp.com/attachments/1099140243724177579/1099255409581236256/cute-pouting.gif",
+        "https://cdn.discordapp.com/attachments/1099140243724177579/1099255409216344136/anime-fan27-pout.gif",
+        "https://cdn.discordapp.com/attachments/1099140243724177579/1099255411061817406/pouts-pout.gif",
+        "https://cdn.discordapp.com/attachments/1099140243724177579/1099255410671755294/pouting-angry.gif",
+        "https://cdn.discordapp.com/attachments/1099140243724177579/1099255410336223283/pout-anime.gif",
+]
+
+PUNCH = ["https://cdn.discordapp.com/attachments/1097837597298016286/1099942758296338452/anime-rezero.gif",
+        "https://cdn.discordapp.com/attachments/1097837597298016286/1099942758690607144/anya-forger-damian-spy-x-family.gif",
+        "https://cdn.discordapp.com/attachments/1097837597298016286/1099942759013548093/kimihito-monster-musume.gif",
+        "https://cdn.discordapp.com/attachments/1097837597298016286/1099942759391051837/one-punch-man-saitama.gif",
+        "https://cdn.discordapp.com/attachments/1097837597298016286/1099942759772717107/rimuru-rimuru-punch.gif",
+        "https://cdn.discordapp.com/attachments/1097837597298016286/1099942760141832242/rin243109-blue-exorcist.gif",
+]
+
+RIP =  ["https://cdn.discordapp.com/attachments/1097839574870405164/1099215525537656892/anime-alone.gif",
+        "https://cdn.discordapp.com/attachments/1097839574870405164/1099215525881581598/anime-emotionless.gif",
+        "https://cdn.discordapp.com/attachments/1097839574870405164/1099215526242299934/exhausted-tired.gif",
+        "https://cdn.discordapp.com/attachments/1097839574870405164/1099215526636560434/falling-over.gif",
+        "https://cdn.discordapp.com/attachments/1097839574870405164/1099215526988877834/kanokari-anime-fall.gif",
+        "https://cdn.discordapp.com/attachments/1097839574870405164/1099215527412510814/reina-aharen-aharen-san-wa-hakarenai.gif",
+        "https://cdn.discordapp.com/attachments/1097839574870405164/1099215527743852544/suicide.gif",
+]
+
+SLAP = ["https://cdn.discordapp.com/attachments/1097837550871248896/1098197475317256242/anime-slap_1.gif",
+        "https://cdn.discordapp.com/attachments/1097837550871248896/1098197475694760026/girl-slap.gif",
+        "https://cdn.discordapp.com/attachments/1097837550871248896/1098197476130955348/shounen-maid-cat.gif",
+        "https://cdn.discordapp.com/attachments/1097837550871248896/1098197476487467117/slap_1.gif",
+        "https://cdn.discordapp.com/attachments/1097837550871248896/1098197476839804970/slap_2.gif",
+        "https://cdn.discordapp.com/attachments/1097837550871248896/1098197477192114226/slap.gif",
+        "https://cdn.discordapp.com/attachments/1097837550871248896/1098197477615734825/slap-bet-chisa-iori.gif",
+        "https://cdn.discordapp.com/attachments/1097837550871248896/1098197478911791114/slap-bitch-slap.gif",
+        "https://cdn.discordapp.com/attachments/1097837550871248896/1098197479620616273/tsuki-tsuki-ga.gif",
+]
+
+SMUG = ["https://cdn.discordapp.com/attachments/1098468936338649168/1099951975300349993/anime-eyebrow-raise.gif",
+        "https://cdn.discordapp.com/attachments/1098468936338649168/1099951975652659332/anime-konata.gif",
+        "https://cdn.discordapp.com/attachments/1098468936338649168/1099951976042741870/futakana-smug-anime.gif",
+        "https://cdn.discordapp.com/attachments/1098468936338649168/1099951976353116251/gawr-gura-smug.gif",
+        "https://cdn.discordapp.com/attachments/1098468936338649168/1099951976705429525/kroni-smug-face.gif",
+        "https://cdn.discordapp.com/attachments/1098468936338649168/1099951977024208967/panda-clip-smug.gif",
+]
+
+SPANK = ["https://cdn.discordapp.com/attachments/1097782168194912296/1098254998179151952/spank-bunny-naughty-bunny.gif",
+        "https://cdn.discordapp.com/attachments/1097782168194912296/1098254997797490819/spank4.gif",
+        "https://cdn.discordapp.com/attachments/1097782168194912296/1098254997487104080/spank.gif",
+        "https://cdn.discordapp.com/attachments/1097782168194912296/1098254996933443735/spank_3.gif",
+        "https://cdn.discordapp.com/attachments/1097782168194912296/1098254996023300229/spank_1.gif",
+        "https://cdn.discordapp.com/attachments/1097782168194912296/1098254995645792296/rikka-takanashi-bad-girl.gif",
+        "https://cdn.discordapp.com/attachments/1097782168194912296/1098254995289297016/bad-spank.gif",
+        "https://cdn.discordapp.com/attachments/1097782168194912296/1098254994928578591/asobi-asobase-school-girl.gif",
+        "https://cdn.discordapp.com/attachments/1097782168194912296/1098254994379112508/anime-school-girl.gif",
+        "https://cdn.discordapp.com/attachments/1097782168194912296/1098254993989054545/anime-girl.gif",
+]
+
+STARE = ["https://cdn.discordapp.com/attachments/1099140258781724673/1099941233108983818/anime-naruto.gif",
+        "https://cdn.discordapp.com/attachments/1099140258781724673/1099941233515835422/anime-sword-art-online.gif",
+        "https://cdn.discordapp.com/attachments/1099140258781724673/1099941233931079752/anime-what.gif",
+        "https://cdn.discordapp.com/attachments/1099140258781724673/1099941234300166206/lamy-stare-anime-stare.gif",
+        "https://cdn.discordapp.com/attachments/1099140258781724673/1099941234698637312/maika-blend-s.gif",
+        "https://cdn.discordapp.com/attachments/1099140258781724673/1099941235105472603/nichijou-blank-stare.gif",
+        "https://cdn.discordapp.com/attachments/1099140258781724673/1099941235453603911/rin-anime.gif",
+]
+
+TICKLE = ["https://cdn.discordapp.com/attachments/1099140658675073104/1099938818976661537/tickle-laugh.gif",
+        "https://cdn.discordapp.com/attachments/1099140658675073104/1099938818662092821/saikin-yatotta-maid-ga-ayashii-anime-tickle.gif",
+        "https://cdn.discordapp.com/attachments/1099140658675073104/1099938818326532117/neptune.gif",
+        "https://cdn.discordapp.com/attachments/1099140658675073104/1099938817999388745/date-a-live-date-a-live-iv.gif",
+        "https://cdn.discordapp.com/attachments/1099140658675073104/1099938817659637770/cute-anime.gif",
+        "https://cdn.discordapp.com/attachments/1099140658675073104/1099938817068253234/anime-tickle.gif",
+        "https://cdn.discordapp.com/attachments/1099140658675073104/1099938816669782086/anime-cat-girl-anime.gif",
+]
