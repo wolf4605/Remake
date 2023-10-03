a539ca1209134ab0f24beb426e7d8e224d6a9648
wolf4605 <125435168+wolf4605@users.noreply.github.com>
2023-10-03 12:03:44 +0000

    Setup file for the cog

--- command/Setup.py
+++ command/Setup.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from command.fun_commands.FUN_1 import FUN
+from command.fun_commands.FUN_1 import Fun_Commands
 
 async def setup(bot:commands.Bot):
-    await bot.add_cog(FUN(bot))
+    await bot.add_cog(Fun_Commands(bot))
